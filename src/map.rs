6e21a5d02e8bde823974c674533d282974687245
jmcph4 <jmcph4.github@gmail.com>
2020-05-23 14:00:02 +1000

    Change map getters to return `Option`s
    This commit makes both `get` and `get_mut` in the `Map` type return
    `Option` types.

--- src/map.rs
+++ src/map.rs
@@ -3,8 +3,8 @@ use crate::error::EnceladusError;
 pub trait Map<K: Sized + Eq + Clone, V: Sized + Eq + Clone>: IntoIterator +
     Eq + Clone {
     fn new() -> Self;
-    fn get(&self, key: K) -> Result<&V, EnceladusError>;
-    fn get_mut(&mut self, key: K) -> Result<&mut V, EnceladusError>;
+    fn get(&self, key: K) -> Result<Option<&V>, EnceladusError>;
+    fn get_mut(&mut self, key: K) -> Result<Option<&mut V>, EnceladusError>;
     fn set(&mut self, key: K, value: V) -> Result<(), EnceladusError>;
     fn remove(&mut self, key: K) -> Result<(), EnceladusError>;
     fn size(&self) -> Result<usize, EnceladusError>;
