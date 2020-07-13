1a3c24e1471d44fa681de5e032f7316150862814
jmcph4 <jmcph4.github@gmail.com>
2020-07-13 21:27:58 +1000

    Define `Tree` implementation stubs
    This commit defines stub implementations for all required functions
    under the `Tree` trait for the `ArrayTree` type.

--- src/arraytree.rs
+++ src/arraytree.rs
@@ -1,6 +1,10 @@
 use std::fmt;
 use std::fmt::{Display, Debug};
 
+use crate::error::EnceladusError;
+use crate::graph::{VertexNumber, EdgeNumber};
+use crate::tree::Tree;
+
 #[derive(Clone, Debug)]
 struct ArrayTreeNode<V, E> {
     vlabel: V,
@@ -57,6 +61,133 @@ impl<V, E> fmt::Display for ArrayTree<V, E> where
             Some(root) => write!(f, "{}", root.vlabel),
             None => write!(f, "null")
         }
+    }
+}
+
+#[allow(unused)]
+impl<V, E> Tree<V, E> for ArrayTree<V, E> where
+    V: Sized + Clone + Eq + Display + Debug,
+    E: Sized + Clone + Eq + Display + Debug {
+    fn new() -> Self {
+        unimplemented!()
+    }
+
+    fn get_vertex(&self, vertex: VertexNumber) ->
+        Result<Option<&V>, EnceladusError> {
+        unimplemented!()
+    }
+
+    fn get_mut_vertex(&mut self, vertex: VertexNumber) ->
+        Result<Option<&mut V>, EnceladusError> {
+        unimplemented!()
+    }
+
+    fn set_vertex(&mut self, vertex: VertexNumber, label: V) ->
+        Result<(), EnceladusError> {
+        unimplemented!()
+    }
+
+    fn get_edge(&self, edge: EdgeNumber) -> Result<Option<&E>, EnceladusError> {
+        unimplemented!()
+    }
+
+    fn get_mut_edge(&mut self, edge: EdgeNumber) ->
+        Result<Option<&mut E>, EnceladusError> {
+        unimplemented!()
+    }
+
+    fn set_edge(&mut self, edge: EdgeNumber, label: E) ->
+        Result<(), EnceladusError> {
+        unimplemented!()
+    }
+
+    fn insert_vertex(&mut self, parent: Option<VertexNumber>, vertex_label: V,
+        edge_label: E) -> Result<VertexNumber, EnceladusError> {
+        unimplemented!()
+    }
+
+    fn remove_vertex(&mut self, vertex: VertexNumber) ->
+        Result<(), EnceladusError> {
+        unimplemented!()
+    }
+
+    fn depth(&self, vertex: VertexNumber) -> Result<usize, EnceladusError> {
+        unimplemented!()
+    }
+
+    fn height(&self, vertex: VertexNumber) -> Result<usize, EnceladusError> {
+        unimplemented!()
+    }
+
+    fn parent(&self, vertex: VertexNumber) ->
+        Result<Option<VertexNumber>, EnceladusError> {
+        unimplemented!()
+    }
+
+    fn children(&self, vertex: VertexNumber) ->
+        Result<Vec<VertexNumber>, EnceladusError> {
+        unimplemented!()
+    }
+
+    fn order(&self) -> Result<usize, EnceladusError> {
+        unimplemented!()
+    }
+ 
+    fn size(&self) -> Result<usize, EnceladusError> {
+        unimplemented!()
+    }
+
+    
+    fn degree(&self, vertex: VertexNumber) -> Result<usize, EnceladusError> {
+        unimplemented!()
+    }
+
+    fn num_children(&self, vertex: VertexNumber) ->
+        Result<usize, EnceladusError> {
+        unimplemented!()
+    }
+
+    fn arity(&self) -> Result<usize, EnceladusError> {
+        unimplemented!()
+    }
+
+    fn is_parent(&self, a: VertexNumber, b: VertexNumber) ->
+        Result<bool, EnceladusError> {
+        unimplemented!()
+    }
+
+    fn is_child(&self, a: VertexNumber, b: VertexNumber) ->
+        Result<bool, EnceladusError> {
+        unimplemented!()
+    }
+
+    fn is_adjacent(&self, a: VertexNumber, b: VertexNumber) ->
+        Result<bool, EnceladusError> {
+        unimplemented!()
+    }
+
+    fn is_incident(&self, vertex: VertexNumber, edge: EdgeNumber) ->
+        Result<bool, EnceladusError> {
+        unimplemented!()
+    }
+
+    fn parent_edge(&self, vertex: VertexNumber) ->
+        Result<Option<EdgeNumber>, EnceladusError> {
+        unimplemented!()
+    }
+
+    fn child_edges(&self, vertex: VertexNumber) ->
+        Result<Vec<EdgeNumber>, EnceladusError> {
+        unimplemented!()
+    }
+
+    fn endpoints(&self, edge: EdgeNumber) ->
+        Result<(VertexNumber, VertexNumber), EnceladusError> {
+        unimplemented!()
+    }
+
+    fn clear(&mut self) -> Result<(), EnceladusError> {
+        unimplemented!()
     }
 }
 
